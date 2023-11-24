a7abf8c520d0aaf599dff55c684f16f3dbcc2b7e
quangvu0811 <quangvantruong08112002@gmail.com>
2023-11-24 17:08:40 +0700

    fist commit

--- session_2/src/bai_1.java
+++ session_2/src/bai_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class bai_6 {
+public class bai_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("nhap 1 so tu nhien :");
@@ -13,7 +13,6 @@ public class bai_6 {
         } else if ( number %3 !=0 && number % 5 ==0){
             System.out.printf(number + " chi chia het cho 5");
         }
-
     }
 
 }
